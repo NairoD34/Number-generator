e7a5e78a8cca574e405fa3ac6925199f189f1426
Velvet <velvet.aubry@mailo.com>
2023-11-20 12:25:58 +0100

    Nouvelles méthodes generateURL, is connected et redirect ajoutées au dispatcher

--- src/App/Dispatcher.php
+++ src/App/Dispatcher.php
@@ -27,4 +27,40 @@ class Dispatcher
         $controller = new $c();
         $controller->$m();
     }
+
+    /**
+     * Génère une URL avec le nom du controlleur et sa méthode
+     */
+    public static function generateUrl(string $controllerName="", string $method="", ?string $query=null):string
+    {
+        if (empty($controllerName) && empty($method)) {
+            return 'index.php';
+        }
+        $url = 'index.php?controller='.$controllerName.'&method='.$method;
+        if (is_array($query) && count($query)>0) {
+            foreach($query as $key=>$value) {
+                $url = $url.'&'.$key.'='.$value;
+            }
+        }
+        return $url;
+    }
+
+    /**
+     * Redirige sur la page indiquée
+     */
+    public static function redirect($controllerName,$method)
+    {
+        header('location: '.self::generateUrl($controllerName,$method));
+    }
+
+    /**
+     * Vérifie si l'user est connectéx
+     */
+    public static function is_connected()
+    {
+        if (isset($_SESSION['connected'])) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/View/header.php
+++ src/View/header.php
@@ -1,7 +1,12 @@
+<?php 
+use vendor\jdl\App\Dispatcher;
+?>
+
 <header>
     <nav>
         <ul>
-            <li><a href="index.php?controller=UtilisateurController&method=displayCreateUtilisateur">S'enregistrer</a></li>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="<?php echo Dispatcher::generateUrl("UtilisateurController", "displayCreateUtilisateur"); ?>">S'enregistrer</a></li>
         </ul>
     </nav>
 </header>
