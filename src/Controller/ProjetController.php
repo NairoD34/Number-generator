fcf425c9c1d9d04d9beb27338d8f00ffe3860974
Lilya EMAD <lilya.emad34@icloud.com>
2023-11-20 15:01:45 +0100

    methode creation d'un projet

---
+++ src/Controller/ProjetController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace vendor\jdl\Controller;
+
+use vendor\jdl\App\AbstractController;
+use vendor\jdl\App\Model;
+use vendor\jdl\Form\ProjetForm;
+
+class ProjetController extends AbstractController
+{
+    public function displayProjet()
+    {
+        // echo 'coucou';
+        $result = Model::getInstance()->readAll('projet');
+        $this->render('projets.php', ['projets'=> $result]);
+    }
+
+    public function createProjet()
+    {
+        if (isset($_POST['submit'])) {
+            $datas = [
+                'nom_projet' => $_POST['nom_projet'],
+                'id_utilisateur'=> 1,   
+                // passer par session et pour attribuer le projet a la session qui en crée un
+
+            ];
+
+            Model::getInstance()->save('projet' , $datas);
+
+            $this->displayProjet();
+        } else {
+            $this->render('projet.php', ['form' => ProjetForm::formProjet('?controller=ProjetController&method=createProjet')]);
+        }
+    }
+}
--- src/View/index.php
+++ src/View/index.php
@@ -5,6 +5,7 @@
     <?php
     include_once(__DIR__ . '/header.php');
     ?>
+    <a href="index.php?controller=ProjetController&method=displayProjet"> Projet : </a><br>
 </body>
 
 </html>
