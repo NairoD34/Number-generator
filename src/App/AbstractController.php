fa2037bb32e7f1c06278d9d2eee4b73e41a2c115
Velvet <velvet.aubry@mailo.com>
2023-11-20 12:50:29 +0100

    render modifié, view/head et index.php modifiés

--- src/App/AbstractController.php
+++ src/App/AbstractController.php
@@ -14,7 +14,14 @@ class AbstractController
 
     public function render($view, $vars)
     {
+        if (empty($vars["title"])) {
+            $vars["title"] = "Tarpin Projet Manager";
+        }
         extract($vars);
-        include_once(__DIR__ . '/../view/' . $view);
+        echo "<!DOCTYPE html><html>";
+        include_once(__DIR__ . '/../View/head.php');
+        include_once(__DIR__ . '/../View/header.php');
+        include_once(__DIR__ . '/../View/' . $view);
+        echo "</html>";
     }
 }
