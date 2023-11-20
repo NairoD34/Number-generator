0957099bef39512be9a2bca2b8bb6f32d3ee3268
NairoD34 <peiro.dorian@gmail.com>
2023-11-20 12:19:28 +0100

    petit resolve pb

--- src/View/header.php
+++ src/View/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul>
-            <li><a href="http://localhost/phpobjet/Projet-2-PHP/?controller=UtilisateurController&method=displayCreateUtilisateur">S'enregistrer</a></li>
+            <li><a href="http://localhost/phpobjet/Projet-PHP/?controller=UtilisateurController&method=displayCreateUtilisateur">S'enregistrer</a></li>
 
         </ul>
     </nav>
