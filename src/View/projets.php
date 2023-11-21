8bf4fd42d5aefcd2cc4e688124e24ae0d622832f
Lilya EMAD <lilya.emad34@icloud.com>
2023-11-21 12:05:30 +0100

    Modification view projet

--- src/View/projets.php
+++ src/View/projets.php
@@ -2,40 +2,22 @@
 <?php
 
 use vendor\jdl\App\Dispatcher;
+use vendor\jdl\Form\UtilisateurForm;
+
 
 if (Dispatcher::is_connected()) {
     echo '<a href="index.php?controller=ProjetController&method=createProjet"> Ajouter un projet : </a><br>';
     // ajouter le foreach ici 
-    var_dump($_SESSION);
+    // var_dump($_SESSION);
     foreach ($projets as $value){
-        // echo '<ul><li><a href="index.php?controller=ProjetController&method=displayProjets&id='
-        // .$value->getId_projet(). '">'
-        // .$value->getNom_projet()."</a></li></ul>";
+
     
         if ($_SESSION['id'] == $value->getId_projet())
         {
             // ajouter un if avec la table participe
-            echo $value->getNom_projet().PHP_EOL;
+
+            echo '<a href='.Dispatcher::generateUrl('ProjetController', 'displayProjet') . '&id=' . $value->getId_projet() .'>' . $value->getNom_projet() . '</a>';
+
         }
     }
-}
-
-//else {
-    // Dispatcher::redirect();
-    //echo 'toto'; // faire un redirect vers l'index
-//}
-
-// ajouter pour voir ses projets lié a son propres id si on est admin
-// afficher les projet lié a l'id utilisateur connecté 
-
-// foreach ($projets as $value){
-//     echo '<ul><li><a href="index.php?controller=ProjetController&method=displayProjets&id='
-//     .$value->getId_projet(). '">'
-//     .$value->getNom_projet()."</a></li></ul>";
-
-
-foreach ($projets as $value){
-    echo '<ul><li><a href="index.php?controller=ProjetController&method=displayProjets&id='
-    .$value->getId_projet(). '">'
-    .$value->getNom_projet()."</a></li></ul>";
 }
