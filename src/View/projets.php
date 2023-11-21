9702cdcb6e7cd6eb01cb7489c4b5bffe9efe8a14
Lilya EMAD <lilya.emad34@icloud.com>
2023-11-21 12:21:27 +0100

    coucou

--- src/View/projets.php
+++ src/View/projets.php
@@ -14,10 +14,10 @@ if (Dispatcher::is_connected()) {
     
         if ($_SESSION['id'] == $value->getId_projet())
         {
-            // ajouter un if avec la table participe
+            // ajouter un if avec la table participe 
 
             echo '<a href='.Dispatcher::generateUrl('ProjetController', 'displayProjet') . '&id=' . $value->getId_projet() .'>' . $value->getNom_projet() . '</a>';
-
+            echo 'coucou';
         }
     }
 }
