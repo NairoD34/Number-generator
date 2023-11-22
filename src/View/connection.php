0ed68012f54a80c6ce2da38884d0293095bf8d9c
Velvet <velvet.aubry@mailo.com>
2023-11-22 10:48:05 +0100

    légères midif du connection.php

--- src/View/connection.php
+++ src/View/connection.php
@@ -1,10 +1,7 @@
 <?php
 
-use vendor\jdl\Form\UtilisateurForm;
-use vendor\jdl\App\Dispatcher;
-
 echo "<h1>Connectez-vous</h1>";
-echo UtilisateurForm::formConnect(Dispatcher::generateUrl("UtilisateurController", "displayConnectUtilisateur"));
+echo $form;
 
 if (!empty($error)) {
   echo "<pre>$error</pre>";
