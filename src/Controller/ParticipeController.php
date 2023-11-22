39e1884fc022f42446786e99d3502db12f2807d4
NairoD34 <peiro.dorian@gmail.com>
2023-11-22 10:20:38 +0100

    debut addusertoproject

---
+++ src/Controller/ParticipeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace vendor\jdl\Controller;
+
+use vendor\jdl\App\AbstractController;
+
+class ParticipeController extends AbstractController
+{
+    public function addUtilisateurToProjet()
+    {
+    }
+}
