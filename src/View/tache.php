7932425e39ec618a55dc97e4ed3bcca16bbfd7e3
Lilya EMAD <lilya.emad34@icloud.com>
2023-11-24 11:21:59 +0100

    création view modifier un user sur tache

--- src/View/tache.php
+++ src/View/tache.php
@@ -4,19 +4,23 @@ use vendor\jdl\App\Dispatcher;
 use vendor\jdl\App\Security;
 
 
+
 echo "<h1>". $tache->getTitre_tache() . "</h1>";
 //echo "<a href=" . Dispatcher::generateUrl('TacheController', 'createTache', ['id_projet' => $_GET['id_projet']]) . ">Créer une nouvelle tâche</a><br>";
-
 
 echo "Description de la tâche : " . $tache->getDescription() . "<br>";
 echo "Priorité de la tâche : " . $priorite . "<br>";
 echo "Statut de la tâche : " . $cdv . "<br>";
 echo "Personne rattaché à la tache : " . $utilisateurs;
 
-if ($isAdmin){
+// if ($isAdmin()){
     echo "<p>(ADMIN)</p>";
-    echo '<a href='. Dispatcher::generateUrl('Controller', '', ['&id_tache=' . $tache->getId_tache()]) . '>Modifier le participant à la tâche</a><br>';
-}
+    echo '<a href='. Dispatcher::generateUrl('TacheController', 'updateUtilisateurToTache', [
+        'id_projet' => $_GET['id_projet'],
+        'id_tache' => $_GET['id_tache'],
+
+        ]). '>Modifier le participant à la tâche</a><br>';
+// }
 
 // changer la personne rattaché a la tache 
 // regarder le form pour ajouter un participant 
--- src/View/updateutilisateurtotache.php
+++ src/View/updateutilisateurtotache.php
@@ -4,3 +4,6 @@ use vendor\jdl\App\Dispatcher;
 
 echo "<h1>Modifier l'utilisateur sur la tâche</h1>";
 echo $form;
+if(!empty($error)){
+    echo $error;
+}
