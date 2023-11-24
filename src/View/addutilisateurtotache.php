b0f4cbddb4446872f9153814b491c0985ae5f143
Lilya EMAD <lilya.emad34@icloud.com>
2023-11-24 10:12:04 +0100

    ajout de la view modifier l'utilisateur sur une tache

---
+++ src/View/addutilisateurtotache.php
@@ -0,0 +1,6 @@
+<?php 
+
+use vendor\jdl\App\Dispatcher;
+
+echo "<h1>Modifier l'utilisateur sur la tâche</h1>";
+echo $form;
--- src/View/tache.php
+++ src/View/tache.php
@@ -1,6 +1,8 @@
 <?php
 
 use vendor\jdl\App\Dispatcher;
+use vendor\jdl\App\Security;
+
 
 echo "<h1>". $tache->getTitre_tache() . "</h1>";
 //echo "<a href=" . Dispatcher::generateUrl('TacheController', 'createTache', ['id_projet' => $_GET['id_projet']]) . ">Créer une nouvelle tâche</a><br>";
@@ -10,3 +12,13 @@ echo "Description de la tâche : " . $tache->getDescription() . "<br>";
 echo "Priorité de la tâche : " . $priorite . "<br>";
 echo "Statut de la tâche : " . $cdv . "<br>";
 echo "Personne rattaché à la tache : " . $utilisateurs;
+
+if ($isAdmin){
+    echo "<p>(ADMIN)</p>";
+    echo '<a href='. Dispatcher::generateUrl('Controller', '', ['&id_tache=' . $tache->getId_tache()]) . '>Modifier le participant à la tâche</a><br>';
+}
+
+// changer la personne rattaché a la tache 
+// regarder le form pour ajouter un participant 
+// ajout un btn modif qui redirige vers le form 
+// changer l'id utilisateur sur la table tache 
